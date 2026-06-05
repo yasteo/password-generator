a04daeef016ba301195b6a7531a2f124a9d7ab93
yasteo <forgeforceandgames@mail.ru>
2026-06-06 00:25:48 +0300

    Unique added

--- src/Generator.java
+++ src/Generator.java
@@ -1,8 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
 public class Generator {
 
     private final String serviceName;
     private final int serviceLength;
     private final int number;
+    private static final int unique;
+    private final int PASSWORD_LENGTH = 16;
+    private static final int MIN_UNIQUE = 1;
+    private static final int MAX_UNIQUE = 8193;
+
+    static {
+        var result = 0;
+        var path = Path.of("num.txt");
+        if (!Files.exists(path)) {
+            try {
+                Files.createFile(path);
+            } catch (IOException e) {
+                System.out.println("Не удалось создать файл");
+            }
+            var random = new Random();
+            try {
+                Files.writeString(path, String.valueOf(random.nextInt(MIN_UNIQUE, MAX_UNIQUE)));
+            } catch (IOException e) {
+                System.out.println("Не удалось напечать число в файл");
+            }
+        }
+        try {
+            var text = Files.readAllLines(path);
+            result = Integer.parseInt(text.getFirst());
+        } catch (IOException e) {
+            System.out.println("Не удалось прочитать файл");
+        } catch (NumberFormatException e) {
+            System.out.println("Неверный формат числа в файле");
+        }
+        unique = result;
+    }
 
     public Generator(String serviceName, int number) {
         this.serviceName = serviceName;
@@ -14,7 +50,7 @@ public class Generator {
         String allowedChars = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*-_+=";
         int len = allowedChars.length();
         var builder = new StringBuilder();
-        for (int i = 0; i < 16; ++i) {
+        for (int i = 0; i < PASSWORD_LENGTH; ++i) {
             int idx = calculateIndex(serviceName.charAt(i % serviceLength), i + 1, len);
             builder.append(allowedChars.charAt(idx));
         }
@@ -22,8 +58,7 @@ public class Generator {
     }
 
     private int calculateIndex(char c, int x, int len) {
-        int idx = this.number * (x * x * x) + this.serviceLength * (x * x) + len * x + (int) c;
-        return idx % len;
+        long idx = (long) this.number * x   * x * x + (long) this.serviceLength * x * x + (long) unique * x + (long) c;
+        return (int) Math.floorMod(idx, len);
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,33 @@ public class Main {
                 try {
                     out.write("Введите название сервиса: ");
                     out.flush();
-                    service = in.readLine();
+                    service = in.readLine().trim();
+                    if (service.isBlank()) {
+                        throw new IllegalStateException("Пустое название сервиса");
+                    }
                     break;
                 } catch (IOException e) {
                     System.out.println("Ошибка ввода/вывода");
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
                 }
             }
             while (true) {
                 try {
                     out.write("Введите ваше число: ");
                     out.flush();
-                    var input = in.readLine();
+                    var input = in.readLine().trim();
                     number = Integer.parseInt(input);
+                    if (number < 0) {
+                        throw new IllegalStateException("Число должно быть больше нуля");
+                    }
                     break;
                 } catch (IOException e) {
                     System.out.println("Ошибка ввода/вывода");
                 } catch (NumberFormatException e) {
-                    System.out.println("Указано неправильное число");
+                    System.out.printf("Число должно быть меньше, чем %d\n", Integer.MAX_VALUE);
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
                 }
             }
             var generator = new Generator(service, number);
@@ -43,16 +53,13 @@ public class Main {
                 try {
                     out.write("Хотите продолжить? [Y/N]: ");
                     out.flush();
-                    userChoice = (char) in.read();
+                    userChoice = in.readLine().trim().charAt(0);
                     in.readLine();
                 } catch (IOException e) {
                     System.out.println("Ошибка ввода/вывода");
                 }
             }
             if (userChoice == 'n' || userChoice == 'N') {
-                break;
-            }
-            if (userChoice != 'y' && userChoice != 'Y') {
                 break;
             }
         }
